f94df49616dcdfe898257590ccc647346a8d6e63
Yang, Simon <simon.yang@emc.com>
2018-02-24 11:00:44 +0800

    test

--- app/component/app.vue
+++ app/component/app.vue
@@ -13,6 +13,27 @@
                   </div>
                 </div>
           </div>
+          <vue-instant
+          :suggestion-attribute="suggestionAttribute"
+          v-model="value"
+          :disabled="false"
+          @input="changed"
+          @click-input="clickInput"
+          @click-button="clickButton"
+          @selected="selected"
+          @enter="enter"
+          @key-up="keyUp"
+          @key-down="keyDown"
+          @key-right="keyRight"
+          @clear="clear"
+          @escape="escape"
+          :show-autocomplete="true"
+          :autofocus="false"
+          :suggestions="suggestions"
+          name="customName"
+          placeholder="custom placeholder"
+          type="spotify">
+          </vue-instant>
         </div>
         <zixuan></zixuan>
       </tab>
@@ -36,19 +57,61 @@ import zixuan from './zixuan.vue';
 import jixuan from './jixuan.vue';
 import notify from './notify.vue';
 import settings from './settings.vue';
-
+import axios from 'axios'
 
 export default {
   data() {
     return {
-      name: 'zixuan'
+      value: '',
+      suggestionAttribute: 'original_title',
+      suggestions: [],
+      selectedEvent: ""
+    }
+  },
+  methods: {
+    clickInput: function() {
+      this.selectedEvent = 'click input'
+    },
+    clickButton: function() {
+      this.selectedEvent = 'click button'
+    },
+    selected: function() {
+      this.selectedEvent = 'selection changed'
+    },
+    enter: function() {
+      this.selectedEvent = 'enter'
+    },
+    keyUp: function() {
+      this.selectedEvent = 'keyup pressed'
+    },
+    keyDown: function() {
+      this.selectedEvent = 'keyDown pressed'
+    },
+    keyRight: function() {
+      this.selectedEvent = 'keyRight pressed'
+    },
+    clear: function() {
+      this.selectedEvent = 'clear input'
+    },
+    escape: function() {
+      this.selectedEvent = 'escape'
+    },
+    changed: function() {
+      var that = this
+      this.suggestions = []
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=342d3061b70d2747a1e159ae9a7e9a36&query=' + this.value)
+        .then(function(response) {
+          response.data.results.forEach(function(a) {
+            that.suggestions.push(a)
+          })
+        })
     }
   },
   components: {
     tabs: tabs,
-    tab:tab,
+    tab: tab,
     zixuan: zixuan,
-    jixuan:jixuan,
+    jixuan: jixuan,
     notify: notify,
     settings: settings
   }
@@ -56,4 +119,5 @@ export default {
 </script>
 
 <style lang="css">
+@import '../../node_modules/vue-instant/dist/vue-instant.css'
 </style>
